d146ee671b6421ff098f36dd427c203e7a623bb5
Jean Zadoreski <zadoreski@gmail.com>
2022-03-28 15:17:31 -0300

    Poo, tratamento de excessão, array

--- cursojava/classesexecutaveis/ArrayVetor.java
+++ cursojava/classesexecutaveis/ArrayVetor.java
@@ -1,18 +1,51 @@
 package cursojava.classesexecutaveis;
 
+import javax.swing.JOptionPane;
+
 public class ArrayVetor {
 	
 	public static void main(String[] args) {
+						   // 0   1    2
+		double[] valores = {8.5,9.6,100.0};
+		String[] teste = {"teste","9.8","7","contado"};
+		String[] valoress = new String[4];
 		
+		//exemplos de tipo de array abaixo
+		int[] inteiro = new int[10];
+		float[] floate = new float[10];
+		
+		valoress[0] = "alex";
+		valoress[1] = "90";
+		valoress[2] = "teste 3 posicao";
+		valoress[3] = "9.5";
+	
+		for (int pos = 0; pos < valoress.length; pos++) {
+			
+			System.out.println("Valor na posição " + (pos + 1) + " é = " + valoress[pos]);
+		}
+		
+		String posicoes = JOptionPane.showInputDialog("Quantas posições o array deve ter? ");
+		
+		//Array pode ser de todos os tipos de dados e objetos tbém
 		//Array sempre deve ter a quantidade de posições definidas
-		double[] notas = new double[4];
+		double[] notas = new double[Integer.parseInt(posicoes)];
 		
-		notas[0] = 7.8;
+		/*notas[0] = 7.8;
 		notas[1] = 8.7;
 		notas[2] = 9.7;
-		notas[3] = 9.9;
+		notas[3] = 9.9;*/
 		
-		System.out.println("teste");
+		for (int pos = 0; pos < notas.length; pos++) {
+			
+			String valor = JOptionPane.showInputDialog("Qual o valor da posição " + (pos + 1));
+			notas[pos] = Double.valueOf(valor);
+			
+		}
+		
+		for (int pos = 0; pos < notas.length; pos++) {
+			
+			System.out.println("Nota " + (pos + 1) + " é = " + notas[pos]);
+		}
 	}
 
 }
